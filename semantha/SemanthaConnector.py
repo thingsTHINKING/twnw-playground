5eab8764023705739870eadd449733f03b4287ab
Niko <niko.duerr@icloud.com>
2023-04-18 15:14:23 +0200

    Add config for semantic comparison

--- semantha/SemanthaConnector.py
+++ semantha/SemanthaConnector.py
@@ -13,9 +13,12 @@ def _to_text_file(text: str):
 
 
 class SemanthaConnector:
-
-    def __init__(self, server_base_url="http://localhost/tt-platform-server", streamlit_domains_prefix="",
-                 api_key=None):
+    def __init__(
+        self,
+        server_base_url="http://localhost/tt-platform-server",
+        streamlit_domains_prefix="",
+        api_key=None,
+    ):
         self.__sdk = semantha_sdk.login(server_url=server_base_url, key=api_key)
         self.__streamlit_domains_prefix = streamlit_domains_prefix
 
@@ -23,60 +26,81 @@ class SemanthaConnector:
         domains = self.__sdk.domains.get()
         return [d.name for d in domains if self.__streamlit_domains_prefix in d.name]
 
-    def query_library(self, text: str, domain: str, threshold=0.75, max_references=10, tags=None):
-        doc = self.__sdk.domains(domain).references.post(file=_to_text_file(text),
-                                                         similaritythreshold=threshold,
-                                                         maxreferences=max_references,
-                                                         tags=tags if tags is not None else [])
+    def query_library(
+        self, text: str, domain: str, threshold=0.75, max_references=10, tags=None
+    ):
+        doc = self.__sdk.domains(domain).references.post(
+            file=_to_text_file(text),
+            similaritythreshold=threshold,
+            maxreferences=max_references,
+            tags=tags if tags is not None else [],
+        )
         result_dict = {}
         if doc.references:
             for ref in doc.references:
                 result_dict[ref.document_id] = {
                     "doc_name": self.__get_ref_doc_name(ref.document_id, domain),
                     "content": self.__get_document_content(ref.document_id, domain),
-                    "similarity": ref.similarity
+                    "similarity": ref.similarity,
                 }
         return result_dict
 
     def get_library(self, domain: str, tags=None, **kwargs):
-        limit = kwargs.get('limit', None)
+        limit = kwargs.get("limit", None)
         if limit is None:
             offset = None
         else:
-            offset = kwargs.get('offset', 0)
-        ref_doc_coll = self.__sdk.domains(domain).referencedocuments.get(tags=tags, offset=offset, limit=limit)
+            offset = kwargs.get("offset", 0)
+        ref_doc_coll = self.__sdk.domains(domain).referencedocuments.get(
+            tags=tags, offset=offset, limit=limit
+        )
         library = []
         for d in ref_doc_coll.documents:
             library.append(
                 {
                     "doc_name": d.name,
-                    "content": self.__get_document_content(d.id, domain)
+                    "content": self.__get_document_content(d.id, domain),
                 }
             )
         return library
 
-    def do_semantic_string_compare(self, input_0: str, input_1: str, domain: str) -> float:
+    def do_semantic_string_compare(
+        self, input_0: str, input_1: str, domain: str
+    ) -> float:
         doc = self.__get_references(input_0, input_1, domain)
         if doc.references:
             return doc.references[0].similarity
         return 0.0
 
     def get_opposite_meaning(self, input_0: str, input_1: str, domain: str) -> bool:
-        doc = self.__get_references(input_0, input_1, domain, "23d06b42-4a32-4531-b00e-640f538e2aee")
-        return doc.pages[0].contents[0].paragraphs[0].references \
-            and doc.pages[0].contents[0].paragraphs[0].references[0].has_opposite_meaning
+        doc = self.__get_references(
+            input_0, input_1, domain, "23d06b42-4a32-4531-b00e-640f538e2aee"
+        )
+        return (
+            doc.pages[0].contents[0].paragraphs[0].references
+            and doc.pages[0]
+            .contents[0]
+            .paragraphs[0]
+            .references[0]
+            .has_opposite_meaning
+        )
 
     def change_model(self, domain: str, model_id: int) -> int:
-        return int(self.__sdk.domains(domain).settings
-                   .patch(PatchDomainSettings(similarity_model_id=str(model_id))).similarity_model_id)
+        return int(
+            self.__sdk.domains(domain)
+            .settings.patch(PatchDomainSettings(similarity_model_id=str(model_id)))
+            .similarity_model_id
+        )
 
     def __get_references(self, input_0, input_1, domain, doc_type=None):
-        return self.__sdk.domains(domain).references.post(file=_to_text_file(input_0),
-                                                          referencedocument=_to_text_file(input_1),
-                                                          similaritythreshold=0.01,
-                                                          maxreferences=1,
-                                                          tags=[],
-                                                          documenttype=doc_type)
+        return self.__sdk.domains(domain).references.post(
+            file=_to_text_file(input_0),
+            referencedocument=_to_text_file(input_1),
+            similaritythreshold=0.01,
+            maxreferences=1,
+            tags=[],
+            documenttype=doc_type,
+        )
 
     def __get_document_content(self, doc_id: str, domain: str) -> str:
         doc = self.__get_ref_doc(doc_id, domain)
--- subpage/semantic_compare.py
+++ subpage/semantic_compare.py
@@ -1,19 +1,18 @@
+import ast
+import os
 import streamlit as st
 from abstract_page import SemanthaBasePage
+from data.read_config import read_config
+
+__config_path = os.path.join("semantic_compare", "config.toml")
+CONFIG = read_config(__config_path)
 
 
 class SemanticCompare(SemanthaBasePage):
-
-    __MODELS = {
-        "Impetuous Shakespeare (ML)": 9021,
-        "Careful Shakespeare (ML)": 19,
-        "Austen (EN)": 9001,
-        "Fontane (DE)": 9002
-    }
-
     def __init__(self):
         super().__init__("🦸🏼‍♀️ Direct Semantic Compare")
-        self.__compare_domain = "PG_Compare"
+        self.__models = ast.literal_eval(self.CONFIG["models"]["names"])
+        self.__compare_domain = CONFIG["domain"]["name"]
 
     def build(self):
         st.markdown("## Direct Compare")
@@ -21,30 +20,41 @@ class SemanticCompare(SemanthaBasePage):
             "Directly compare two texts by entering them below. The texts will be compared using semantha's® semantic model. The similarity score is a value between 0 and 1. The higher the score, the more similar the texts are."
         )
         curr_model = st.selectbox(
-            "Which model would you like to use?",
-            (self.__MODELS.keys())
+            "Which model would you like to use?", (self.__models.keys())
         )
-        with st.spinner('Changing the model. Just a second...'):
-            new_model = self._semantha_connector.change_model(self.__compare_domain, self.__MODELS[curr_model])
+        with st.spinner("Changing the model. Just a second..."):
+            _ = self._semantha_connector.change_model(
+                self.__compare_domain, self.__models[curr_model]
+            )
         input_0 = st.text_input(label="Input I", value="I like to eat apples.")
         input_1 = st.text_input(label="Input II", value="I like to eat bananas.")
-        __do_omd = st.checkbox("Check whether similar sentence have an opposite meaning.", value=False)
+        __do_omd = st.checkbox(
+            "Check whether similar sentence have an opposite meaning.", value=False
+        )
         _, col, _ = st.columns([1, 1, 1])
         if col.button("⇆ Semantic Compare", key="scbutton"):
-            with st.spinner('Wait for it...'):
+            with st.spinner("Wait for it..."):
                 self.compute_and_display_similarity(input_0, input_1, __do_omd)
 
-    def compute_and_display_similarity(self, input_0: str, input_1: str, __do_omd: bool):
+    def compute_and_display_similarity(
+        self, input_0: str, input_1: str, __do_omd: bool
+    ):
         similarity = self._semantha_connector.do_semantic_string_compare(
             input_0, input_1, self.__compare_domain
         )
         sim = int(round(similarity, 2) * 100)
-        __omd = False
-        if __do_omd:
-            __omd = self._semantha_connector.get_opposite_meaning(input_0, input_1, self.__compare_domain)
+        __omd = (
+            False
+            if not __do_omd
+            else self._semantha_connector.get_opposite_meaning(
+                input_0, input_1, self.__compare_domain
+            )
+        )
         if sim >= 70:
             if __do_omd and __omd:
-                st.success(f"🦸🏼‍♀️ The texts are {sim}% similar but have an **opposite meaning**.")
+                st.success(
+                    f"🦸🏼‍♀️ The texts are {sim}% similar but have an **opposite meaning**."
+                )
             else:
                 st.success(f"🦸🏼‍♀️ The texts are {sim}% similar.")
         elif 70 > sim > 40:
